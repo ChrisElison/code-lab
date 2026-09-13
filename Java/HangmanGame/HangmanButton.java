f610180a1e593bbee85c2ba3110843a64b8dfbd8
Christopher David Elison <christopher.d.elison@gmail.com>
2026-09-13 16:58:13 +0100

    Hangman Ep 5 - Buttons!

---
+++ Java/HangmanGame/HangmanButton.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class HangmanButton extends JButton implements ActionListener {
+    public HangmanButton(String label) {
+        this.setPreferredSize(new Dimension(48, 48));
+        this.setText(label);
+        System.out.println("Created button: " + label);
+
+        this.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        this.setEnabled(false);
+        System.out.println("Making guess: " + this.getText().charAt(0));
+    }
+}
